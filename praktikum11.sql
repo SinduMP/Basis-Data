327c4276fd3a616884086993d3b281a63f08de8b
Sindu Masri Priandana <73923369+SinduMP@users.noreply.github.com>
2022-06-22 13:51:18 +0700

    Update praktikum11.sql

--- praktikum11.sql
+++ praktikum11.sql
@@ -1,1 +1,24 @@
 
+--1. Tabel barang
+CREATE TABLE barang 
+(kode char(5), nama varchar(20), jenis varchar(15), harga int, jumlah int, pemasok char(5));
+
+--2. Tabel jual
+CREATE TABLE jual 
+(notrans char(5), kode char(5), jumlah int);
+
+--3. Tabel beli
+CREATE TABLE beli 
+(nofaktur char(5), kode char(5), harga int, jumlah int);
+
+--A. TRIGGER
+--1. TRIGGER insjual_updjumlah
+CREATE TRIGGER insjual_updjumlah
+BEFORE INSERT
+ON jual
+FOR EACH ROW
+BEGIN
+	UPDATE barang SET jumlah = (jumlah - new.jumlah)
+	WHERE kode = new.kode;
+END//
+
